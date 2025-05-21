8cf875c6f57025c4e27c2ab57a1187d37de0861f
Salsabilla Nuhaa Aini <143937129+SALSABILLANUHAAAINI@users.noreply.github.com>
2025-05-21 19:43:35 +0700

    Ensure static files are correctly configured

--- FinalProject/wsgi.py
+++ FinalProject/wsgi.py
@@ -16,5 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinalProject.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application,
-    root=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../staticfiles'))
+    root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'))
 
