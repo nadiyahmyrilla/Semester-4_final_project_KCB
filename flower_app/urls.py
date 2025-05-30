62b688e683bdfd536fa22c3066cadc2a22ee806c
Nadiyah myrilla <143943300+nadiyahmyrilla@users.noreply.github.com>
2025-05-30 15:04:33 +0700

    Update urls.py

--- flower_app/urls.py
+++ flower_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('dataset/', views.dataset, name='dataset'),                  # Navbar 1: index.html
+    path('', views.home, name='home'),                  # Navbar 1: index.html
     path('dataset/', views.dataset, name='dataset'),    # Navbar 2: dataset.html
     path('testing/', views.testing, name='testing'),    # Navbar 3: testing.html
     path('klasifikasi/', views.klasifikasi, name='klasifikasi'),  # Navbar 4: klasifikasi.html
